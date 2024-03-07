6be5acc7f3f9ad8baccf81ab74dbecd51bc4287c
Tejendrasinh6844 <149938061+Tejendrasinh6844@users.noreply.github.com>
2024-03-07 15:45:25 +0000

    wah

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                   This is the movie app Created By TEJENDRA who pasinate in react web dev.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -45,7 +45,7 @@ const HeroBanner = () => {
             <div className="opacity-layer"></div>
             <ContentWrapper>
                 <div className="heroBannerContent">
-                    <span className="title">સ્વાગતમ.</span>
+                    <span className="title">WelCome.</span>
                     <span className="subTitle">
                         Millions of movies, TV shows and people to discover.
                         Explore now.
